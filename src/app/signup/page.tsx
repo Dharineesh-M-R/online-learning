4a0e0048ddc4982e4c12061b3e58553963c3f43c
Dharineesh-M-R <dharineeshmagudeswaran@gmail.com>
2025-06-25 08:37:07 +0530

    asdf

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,19 +7,43 @@ import {Axios} from "axios";
 
 
 export default function signuppage(){
-    const [user, serUser] = React.useState({
+    const [user, setUser] = React.useState({
         email:"",
         password:"",
         username:"",
     })
+    const onSignup = async () => {
+
+    }
     return (
         <div>
             <h1>Signup</h1>
             <form action="">
-                <label htmlFor="">Login</label>
-                <input type="email" /><br />
-                <label htmlFor="">Password</label>
-                <input type="password" /><br />
+                <label htmlFor="username">Username :</label> <br />
+                <input 
+                className="p-2 text-black border border-m bg-white placeholder-red-500"
+                id="username" 
+                type="text" 
+                value={user.username}
+                onChange={(e)=>setUser({...user, username: e.target.value})}
+                placeholder="Username"/> <br />
+                <label htmlFor="password">Password :</label> <br />
+                <input 
+                className="p-2 text-black border border-m bg-white placeholder-red-500"
+                id="password" 
+                type="password" 
+                value={user.password}
+                onChange={(e)=>setUser({...user, password: e.target.value})}
+                placeholder="Password"/> <br />
+                <label htmlFor="email">Email :</label> <br />
+                <input 
+                className="p-2 text-black border border-m bg-white placeholder-red-500"
+                id="email" 
+                type="email" 
+                value={user.email}
+                onChange={(e)=>setUser({...user, email: e.target.value})}
+                placeholder="Email"/> <br />
+                
                 <input type="submit" name="Submit" />
             </form>
         </div>
